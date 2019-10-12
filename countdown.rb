2c26eb13ce7d12e29b3e089a4eb8599e0175891b
Justin Miller <jayman1294@gmail.com>
2019-10-12 14:06:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown(number)
+  number = 10
   while  number > 0 
     puts "#{number} SECONDS(S)!"
-    number -=1
+    number -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
