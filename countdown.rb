fcbff5a9f4b1862d3bbe115820eaf1ff5de852d4
Justin Miller <jayman1294@gmail.com>
2019-10-12 14:04:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 
-def countdown(integer)
+def countdown(number)
   until integer == 0 
-    puts "#{integer}"
+    puts "#{number} SECONDS(S)!"
     integer -=1
   end
+  puts "HAPPY NEW YEAR!"
 end
 
 
